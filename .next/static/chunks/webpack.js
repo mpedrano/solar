df2548c4fc477e51fd3b753c3799c3d45ecafd05
mpedrano <mpedrano@my.bcit.ca>
2024-03-06 09:22:32 -0800

    update logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "24d4babd6a080d75"; }
+/******/ 		__webpack_require__.h = function() { return "ceca7bf1a6c996c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
